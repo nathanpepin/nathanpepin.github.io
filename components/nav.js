20b7892064fec1778367acd4523b186d8a2f26fc
nathanpepin <46303125+nathanpepin@users.noreply.github.com>
2019-08-29 00:54:29 -0400

    Add files via upload

---
+++ components/nav.js
@@ -0,0 +1,25 @@
+let html =
+  '<nav>\
+    <div>\
+      <a href="">Home</a>\
+    </div>\
+    <div>\
+      <a href="">Apps</a>\
+    </div>\
+    <div>\
+      <a href="">Podcast</a>\
+    </div>\
+    <div>\
+      <a href="">Music</a>\
+    </div>\
+    <div>\
+      <a href="">Blog</a>\
+    </div>\
+    <div>\
+      <a href="">Portfolio</a>\
+    </div>\
+    <div>\
+      <a href="">Contact</a>\
+    </div>';
+
+document.write(html);
